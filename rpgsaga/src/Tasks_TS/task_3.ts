036a683cd749d690fc16431ec59b93a9c002dd39
Maksim <makoldinm@yandex.ru>
2026-05-23 10:24:00 +0300

    fix_3rd_task

--- rpgsaga/src/Tasks_TS/task_3.ts
+++ rpgsaga/src/Tasks_TS/task_3.ts
@@ -1,43 +1,45 @@
-// А и Б
-const a: number = 4.1;
-const b: number = 2.7;
-
 function calculateY(x: number, a: number, b: number): number {
-    
     const log5_x = Math.log(x) / Math.log(5);
-    
     const numerator = a * Math.sqrt(x) - b * log5_x;
-    
     const denominator = Math.log10(Math.abs(x - 1));
     
     return numerator / denominator;
 }
 
-// задача А
-function TaskA(): void{
-// console.log("____________задача А");
 
-const x_start: number = 1.2; // xн
-const x_end: number = 5.2;   // xк
-const dx: number = 0.8;      // дельта x
+function TaskA(a: number, b: number, x_start: number, x_end: number, dx: number): string[] {
+    const results: string[] = []; // строка для ответа
 
-
-for (let x = x_start; x <= x_end; x += dx) {
-    const y = calculateY(x, a, b);
-    console.log(`x = ${x}, y = ${y}`);
+    for (let x = x_start; x <= x_end; x += dx) {
+        const y = calculateY(x, a, b);
+        
+        results.push(`x = ${x.toFixed(1)}, y = ${y}`); 
+    }
+    
+    return results; // возврат ответа
 }
+
+
+function TaskB(a: number, b: number, x_values: number[]): string[] {
+    const results: string[] = []; 
+
+    for (let i = 0; i < x_values.length; i = i + 1) {
+        const x = x_values[i];
+        const y = calculateY(x, a, b);
+        results.push(`x${i + 1} = ${x}, y = ${y}`);
+    }
+    
+    return results;
 }
-// задача Б
-function TaskB(): void{
-// console.log("____________задача Б");
 
-const x_values: number[] = [1.9, 2.15, 2.34, 2.73, 3.16];
+const a_val: number = 4.1;
+const b_val: number = 2.7;
 
-for (let i = 0; i < x_values.length; i = i+1) {
-    const x = x_values[i];
-    const y = calculateY(x, a, b);
-    console.log(`x${i + 1} = ${x}, y = ${y}`);
-}}
 
-TaskA()
-TaskB()
+console.log("Задача А") // вывод в логах, т.к. запускаю через консоль
+
+console.log(TaskA(a_val, b_val, 1.2, 5.2, 0.8))
+
+console.log("Задача Б")
+
+console.log(TaskB(a_val, b_val, [1.9, 2.15, 2.34, 2.73, 3.16]))
